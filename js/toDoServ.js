b518b6706244d04ce907f1dd9fd70d63f70c6f74
Matthew Langford <m.c.langford@live.com>
2017-03-15 11:43:44 -0600

    minor changes

--- js/toDoServ.js
+++ js/toDoServ.js
@@ -1,20 +1,25 @@
 angular.module('toDoApp').service("toDoServ", function() {
 
-            // Add item function
-            var toDoList = ["Laundry", "Dishes", "Vaccume", "Mop", ] // Default data
-            this.addItem = function(item) {
-                todo.push(item); //add item
-            }
+    // Add item function
+    var toDoList = ["Laundry", "Dishes", "Vaccume", "Mop"]; // Default data
 
-            //remove item function
-            this.removeItem = function(index) {
-                (var i = 0; i < toDoList.length; i++) {
-                    if (toDoList[i] === index) {
-                        toDoList.splice(i, 1)
-                    }
-                }
-                //function for $scope on controller to access toDoList array
-                this.getList = function() {
-                    return toDoList;
-                }
-            });
+    this.addItem = function (item) {
+        todo.push(item); //add item
+    };
+
+    //remove item function
+    this.removeItem = function (index) {
+        for (var i = 0; i < toDoList.length; i++) {
+            if (toDoList[i] === index) {
+                toDoList.splice(i, 1)
+            }
+        }
+    };
+    //function for $scope on controller to access toDoList array
+    this.getList = function () {
+        return toDoList;
+    };
+
+
+});
+
