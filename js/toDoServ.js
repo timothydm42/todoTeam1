c8ed3d3a500612fe25cff351de984457feede85a
Nick Celaya <NickCelaya07@gmail.com>
2017-03-15 11:31:54 -0600

    added service with some functions.

---
+++ js/toDoServ.js
@@ -0,0 +1,20 @@
+angular.module('toDoApp').service("toDoServ", function() {
+
+            // Add item function
+            var toDoList = ["Laundry", "Dishes", "Vaccume", "Mop", ] // Default data
+            this.addItem = function(item) {
+                todo.push(item); //add item
+            }
+
+            //remove item function
+            this.removeItem = function(index) {
+                (var i = 0; i < toDoList.length; i++) {
+                    if (toDoList[i] === index) {
+                        toDoList.splice(i, 1)
+                    }
+                }
+                //function for $scope on controller to access toDoList array
+                this.getList = function() {
+                    return toDoList;
+                }
+            });
